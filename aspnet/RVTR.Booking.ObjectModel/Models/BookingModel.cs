a6831fecc74a6a4dd493e03caa03e162274e6c5a
fred belotte <fredbelotte@outlook.com>
2020-04-25 23:08:49 -0500

    add booking object model [#172282248]

--- aspnet/RVTR.Booking.ObjectModel/Models/BookingModel.cs
+++ aspnet/RVTR.Booking.ObjectModel/Models/BookingModel.cs
@@ -3,8 +3,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RVTR.Booking.ObjectModel.Models
 {
+  /// <summary>
+  /// Represents the _Booking_ model
+  /// </summary>
   public class BookingModel : IValidatableObject
   {
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => throw new System.NotImplementedException();
+    public int Id { get; set; }
+    public int AccountId { get; set; }
+    public int LodgingId { get; set; }
+    public IEnumerable<int> Guests { get; set; }
+    public IEnumerable<int> Rentals { get; set; }
+    public StayModel Stay { get; set; }
+    public string Status { get; set; }
+
+    /// <summary>
+    /// Represents the _Booking_ `Validate` method
+    /// </summary>
+    /// <param name="validationContext"></param>
+    /// <returns></returns>
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => new List<ValidationResult>();
   }
 }
---
+++ aspnet/RVTR.Booking.ObjectModel/Models/StayModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RVTR.Booking.ObjectModel.Models
+{
+  /// <summary>
+  /// Represents the _Stay_ model
+  /// </summary>
+  public class StayModel : IValidatableObject
+  {
+    public int Id { get; set; }
+    public DateTime CheckIn { get; set; }
+    public DateTime CheckOut { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+
+    /// <summary>
+    /// Represents the _Stay_ `Validate` method
+    /// </summary>
+    /// <param name="validationContext"></param>
+    /// <returns></returns>
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => new List<ValidationResult>();
+  }
+}

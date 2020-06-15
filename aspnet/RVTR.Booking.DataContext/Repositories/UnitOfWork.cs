5e26d9dd297f116091e9669226a5a5dbc5b37218
michwong98 <michwong98@gmail.com>
2020-06-15 12:08:58 -0400

    Reverted database changes.

--- aspnet/RVTR.Booking.DataContext/Repositories/UnitOfWork.cs
+++ aspnet/RVTR.Booking.DataContext/Repositories/UnitOfWork.cs
@@ -1,30 +1,45 @@
+using System;
 using System.Threading.Tasks;
 using RVTR.Booking.ObjectModel.Models;
 
 namespace RVTR.Booking.DataContext.Repositories
 {
-  /// <summary>
-  /// Represents the _UnitOfWork_ repository
-  /// </summary>
-  public class UnitOfWork
-  {
-    private readonly BookingContext _context;
-
-    public virtual Repository<BookingModel> Booking { get; }
-    public virtual Repository<StayModel> Stay { get; }
-
-    public UnitOfWork(BookingContext context)
-    {
-      _context = context;
-
-      Booking = new Repository<BookingModel>(context);
-      Stay = new Repository<StayModel>(context);
-    }
-
     /// <summary>
-    /// Represents the _UnitOfWork_ `Commit` method
+    /// Represents the _UnitOfWork_ repository
     /// </summary>
-    /// <returns></returns>
-    public async Task<int> CommitAsync() => await _context.SaveChangesAsync();
-  }
+    public class UnitOfWork
+    {
+        private readonly BookingContext _context;
+
+        private Repository<BookingModel> _booking;
+        public virtual Repository<BookingModel> Booking
+        {
+            get
+            {
+              if (this._booking == null)
+                this._booking = new Repository<BookingModel>(this._context);
+              return _booking;
+            }
+        }
+
+        private Repository<StayModel> _stay;
+        public virtual Repository<StayModel> Stay {
+          get
+          {
+            if (this._stay == null)
+              this._stay = new Repository<StayModel>(this._context);
+            return _stay;
+          }
+        }
+
+        public UnitOfWork(BookingContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Represents the _UnitOfWork_ `Commit` method
+        /// </summary>
+        public async Task<int> CommitAsync() => await _context.SaveChangesAsync();
+    }
 }

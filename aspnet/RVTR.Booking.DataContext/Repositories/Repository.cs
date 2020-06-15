e8aa6d3372dc03a8ffffb09c1dbea56ea71b7077
michwong98 <michwong98@gmail.com>
2020-06-15 12:25:55 -0400

    Get method for repository and unit of work.

--- aspnet/RVTR.Booking.DataContext/Repositories/Repository.cs
+++ aspnet/RVTR.Booking.DataContext/Repositories/Repository.cs
@@ -1,30 +1,52 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 
 namespace RVTR.Booking.DataContext.Repositories
 {
-  /// <summary>
-  /// Represents the _Repository_ generic
-  /// </summary>
-  /// <typeparam name="TEntity"></typeparam>
-  public class Repository<TEntity> where TEntity : class
-  {
-    public readonly DbSet<TEntity> _db;
-
-    public Repository(BookingContext context)
+    /// <summary>
+    /// Represents the _Repository_ generic
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class Repository<TEntity> where TEntity : class
     {
-      _db = context.Set<TEntity>();
+        public readonly DbSet<TEntity> _db;
+
+        public Repository(BookingContext context)
+        {
+            _db = context.Set<TEntity>();
+        }
+
+        public virtual async Task DeleteAsync(int id) => _db.Remove(await SelectAsync(id));
+
+        public virtual async Task InsertAsync(TEntity entry) => await _db.AddAsync(entry).ConfigureAwait(true);
+
+        public virtual async Task<IEnumerable<TEntity>> SelectAsync() => await _db.ToListAsync();
+
+        public virtual async Task<TEntity> SelectAsync(int id) => await _db.FindAsync(id).ConfigureAwait(true);
+
+        public virtual async Task<IEnumerable<TEntity>> SelectAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _db;
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (!string.IsNullOrEmpty(includeProperties))
+                foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public virtual void Update(TEntity entry) => _db.Update(entry);
     }
-
-    public virtual async Task DeleteAsync(int id) => _db.Remove(await SelectAsync(id));
-
-    public virtual async Task InsertAsync(TEntity entry) => await _db.AddAsync(entry).ConfigureAwait(true);
-
-    public virtual async Task<IEnumerable<TEntity>> SelectAsync() => await _db.ToListAsync();
-
-    public virtual async Task<TEntity> SelectAsync(int id) => await _db.FindAsync(id).ConfigureAwait(true);
-
-    public virtual void Update(TEntity entry) => _db.Update(entry);
-  }
 }

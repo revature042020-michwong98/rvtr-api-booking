253c7ede37c7bc6425a10189f1bb184248e56b13
michwong98 <michwong98@gmail.com>
2020-06-19 10:33:12 -0400

    LodgingId filter for stay.

--- aspnet/RVTR.Booking.DataContext/SearchFilters/StaySearchFilter.cs
+++ aspnet/RVTR.Booking.DataContext/SearchFilters/StaySearchFilter.cs
@@ -38,7 +38,7 @@ namespace RVTR.Booking.DataContext
 
         public StaySearchFilter(StaySearchQueries staySearchQueries) : base(staySearchQueries)
         {
-            this.Includes = "Booking";
+            this.Includes = "Booking,Booking.Rentals";
 
             CreateDateFilter(staySearchQueries.Dates);
             CreateLodgingIdFilter(staySearchQueries.LodgingId);

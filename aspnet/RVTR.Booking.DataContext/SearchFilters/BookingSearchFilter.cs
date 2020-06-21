02098523321b7b3ac2d1a04282b686f24a87d6ed
michwong98 <michwong98@gmail.com>
2020-06-21 15:38:02 -0400

    Fixed includes.

--- aspnet/RVTR.Booking.DataContext/SearchFilters/BookingSearchFilter.cs
+++ aspnet/RVTR.Booking.DataContext/SearchFilters/BookingSearchFilter.cs
@@ -49,7 +49,7 @@ namespace RVTR.Booking.DataContext
         public BookingSearchFilter(BookingSearchQueries bookingSearchQueries)
             : base(bookingSearchQueries)
         {
-            Includes = "Guests,Rentals";
+            Includes = "Guests,Rentals,Stay";
 
             CreateAccountIdFilter(bookingSearchQueries?.AccountId);
             CreateLodgingIdFilter(bookingSearchQueries?.LodgingId);
